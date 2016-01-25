39fbbcfbbc6c526f8df227679bd326a2851d260c
Masa Maeda <masamaedae@gmail.com>
2016-01-25 12:23:09 -0800

    Added GameResult enum to have 3 states so we can have win loss and tie

--- Game.cs
+++ Game.cs
@@ -4,8 +4,18 @@ using System.Linq;
 // This class contains logic for playing games
 public static class Game
 {
+	public enum GameResult
+	{
+		// Player 1 wins
+		Win,
+		// Player 2 wins
+		Loss,
+		// It's a tie
+		Tie
+	}
+
 	// Plays a game and returns true if player 1 wins
-	public static bool PlayGame( Player p1, Player p2 )
+	public static GameResult PlayGame( Player p1, Player p2 )
 	{
 		GameBoard g = new GameBoard();
 
@@ -22,7 +32,7 @@ public static class Game
 			if( g.IsWinner( true ) )
 			{
 				Console.WriteLine( "Player 1 Wins!" );
-				return true;
+				return GameResult.Win;
 			}
 
 			g.MakeMove( p2.MakeMove( new GameBoard( g.Board ) ), false );
@@ -33,13 +43,13 @@ public static class Game
 			if( g.IsWinner( false ) )
 			{
 				Console.WriteLine( "Player 2 Wins!" );
-				return false;
+				return GameResult.Loss;
 			}
 
 			if( !g.MovesAvailable() )
 			{
 				Console.WriteLine( "It's a draw!" );
-				return false;
+				return GameResult.Tie;
 			}
 		}
 	}
@@ -56,6 +66,7 @@ public static class Game
 		return choice;
 	}
 
+	// Alows the user to choose which of the player types each player is
 	private static void ManualChoice( Type[] playerTypes, Random r, out Player p1, out Player p2 )
 	{
 		Console.WriteLine( "Choose Player 1's type: " );
@@ -98,7 +109,7 @@ public static class Game
 		Player p1;
 		Player p2;
 
-		// Find all the types that are decendants of player and therefore can be played with
+		// Find all the types that are decendants of player in this assembly and therefore can be played with
 		Type[] playerTypes = typeof( Player ).Assembly.GetTypes().Where( (x) => x.BaseType == typeof( Player ) ).ToArray();
 
 		Console.WriteLine( "Choose gamemode:" );
